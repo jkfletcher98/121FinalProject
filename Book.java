ac941df51790deda06f3865c012baeb79276943f
jkfletcher98 <jkfletcher@bsu.edu>
2025-04-08 13:47:37 +0000

    Started book class

---
+++ Book.java
@@ -0,0 +1,57 @@
+class Book {
+	private String title;
+	private String author;
+	private String genre;
+	private String location;
+
+	public Book(){
+		this.title = "";
+		this.author = "";
+		this.genre = "";
+		this.location = "";
+	} // end constructor
+
+	public Book(String title, String author, String genre, String location){
+		this.title = title;
+		this.author = author;
+		this.genre = genre;
+		this.location = location;
+	} // end constructor
+
+	public void setTitle(String title){
+		this.title = title;
+	} // end setTitle
+
+	public String getTitle(){
+		return this.title;
+	} // end getTitle
+
+	public void setAuthor(String author){
+		this.author = author;
+	} // end setAuthor
+
+	public String getAuthor(){
+		return this.author;
+	} // end getAuthor
+
+	public void setGenre(String genre){
+		this.genre = genre;
+	} // end setGenre
+
+	public String getGenre(){
+		return this.genre;
+	} // end getGenre
+
+	public void setLocation(String location){
+		this.location = location;
+	} // end setGenre
+
+	public String getLocation(){
+		return this.location;
+	} // end getLocation
+
+	public void printBook(){
+		System.out.println(this.title + "by " + this.author + "(" + this.location + ")");
+	} // end printBook
+
+} // end class def
